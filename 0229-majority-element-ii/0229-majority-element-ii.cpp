c90d3e86611f96ef93b512bc27a0f7c28cb68b89
Keshav Mishra <keshav0774@gmail.com>
2026-08-04 14:18:34 +0530

    Time: 0 ms (100%), Space: 23.8 MB (48.43%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -2,40 +2,34 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         
-        int candidate1=0, candidate2=0;
-        int vote1 = 0, vote2 = 0;
-
-        for(int i=0; i<nums.size(); i++){
-            
-            if(candidate1 == nums[i]) vote1++;
-            else if(candidate2 == nums[i]) vote2++;
-            else if(vote1 == 0){
-                candidate1 = nums[i];
-                vote1++;
-            }
-            else if(vote2 == 0){
-                candidate2 = nums[i];
-                vote2++;
-            }
-            else {
-                vote1--;
-                vote2--;
-            }
+     int firstElement = 0, count = 0;
+     int secondElement = 0, vote = 0;
+     for(int i=0; i<nums.size(); i++){
+        if(firstElement == nums[i]) count++;
+        else if(secondElement == nums[i]) vote++;
+        else if(count == 0){
+            firstElement = nums[i];
+            count++;
         }
-        int n = nums.size()/3;
-        vote1 = vote2 = 0;
-        for(int i=0;i<nums.size(); i++){
-            if(nums[i] == candidate1) vote1++;
-          else  if(nums[i] == candidate2) vote2++;
+        else if(vote == 0){
+            secondElement = nums[i];
+            vote++;
         }
-        vector<int> ans;
-
-    if(vote1 > n)
-    ans.push_back(candidate1);
-
-    if(vote2 > n)
-    ans.push_back(candidate2);
-
-     return ans;
+        else {
+            vote--;
+            count--;
+        }
+    }
+    vote = count = 0;
+    for(int i=0; i<nums.size(); i++){
+        if(nums[i] == firstElement) count++;
+        else if(nums[i] == secondElement) vote++;
+    }   
+    int n = nums.size()/3;
+    vector<int>ans;
+    if(count > n) ans.push_back(firstElement);
+    if(vote > n) ans.push_back(secondElement);
+    return ans;
+    
     }
 };
