c47630ea14428afa17da33933b97d532438bd278
Keshav Mishra <keshav0774@gmail.com>
2026-08-09 15:10:07 +0530

    Time: 0 ms (100%), Space: 46.3 MB (34.96%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -2,30 +2,17 @@ class Solution {
 public:
     int maxScore(vector<int>& cardPoints, int k) {
         
-        // int kSum = 0;
-        // for(int i=0; i<k; i++) kSum += cardPoints[i];
-        // if(k == cardPoints.size()) return kSum;
-
-        // int left = k-1;
-        // int right = cardPoints.size()-1;
-        // int maxSum = kSum;
-        // while(left>=0 && right>=0){
-        //     kSum += cardPoints[right] - cardPoints[left]; 
-        //     maxSum = max(maxSum,kSum);
-        //     left--,right--;
-        // }
-        // return kSum;
-        int kSum = 0; 
+        int kSum = 0;
         for(int i=0; i<k; i++) kSum += cardPoints[i];
         if(k == cardPoints.size()) return kSum;
-        
+
         int left = k-1;
-        int n = cardPoints.size()-1;
+        int right = cardPoints.size()-1;
         int maxSum = kSum;
-        while(left>=0 && n>=0){
-            kSum += cardPoints[n] - cardPoints[left]; 
-            maxSum = max(maxSum , kSum);
-            left--,n--;
+        while(left>=0 && right>=0){
+            kSum += cardPoints[right] - cardPoints[left]; 
+            maxSum = max(maxSum,kSum);
+            left--,right--;
         }
         return maxSum;
     }
