f6ac623f78f069a4edb8cae44bfd4f3e3905a4be
Keshav Mishra <keshav0774@gmail.com>
2026-08-23 13:38:22 +0530

    Time: 0 ms (100%), Space: 11.8 MB (79.77%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,17 +1,32 @@
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& strs) {
+    string longestCommonPrefix(vector<string>& nums) {
         
-        sort(strs.begin(), strs.end()); 
+        // sort(strs.begin(), strs.end()); 
 
-        string first = strs.front(); 
-        string last = strs.back(); 
-        int minLength = min(first.size() , last.size());
-        string ans = "";
-        for(int i=0; i<minLength; i++){
-            if(first[i] != last[i]) break;
-            else ans.push_back(first[i]);
+        // string first = strs.front(); 
+        // string last = strs.back(); 
+        // int minLength = min(first.size() , last.size());
+        // string ans = "";
+        // for(int i=0; i<minLength; i++){
+        //     if(first[i] != last[i]) break;
+        //     else ans.push_back(first[i]);
+        // }
+        // return ans;
+                int minLength = nums[0].size(); 
+    for(int i=1; i<nums.size(); i++) minLength = min(minLength , (int)nums[i].size());
+    
+    string ans = "";
+    for(int i=0; i<minLength; i++){
+        
+        char ch = nums[0][i]; 
+        
+        for(int j=1; j<nums.size(); j++){
+            if(nums[j][i] != ch) return ans;
         }
-        return ans;
+        
+        ans.push_back(ch);
+    }
+    return ans;
     }
 };
