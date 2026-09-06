5505a8818dd669852eb257c8d02f53c31cbc1b3f
Keshav Mishra <keshav0774@gmail.com>
2026-09-06 17:01:41 +0530

    Time: 0 ms (100%), Space: 9.3 MB (62.46%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        
+        vector<int>last(26,0);
+        for(int i=0; i< s.size(); i++) last[s[i] - 'a'] = i;
+
+        vector<bool>mark(26,false);
+        string ans;
+        for(int i=0; i<s.size(); i++){
+
+            if(mark[s[i]-'a']) 
+                continue; 
+
+            while(!ans.empty() && ans.back() > s[i] && last[ans.back() -'a'] > i){
+                mark[ans.back() - 'a'] = false;
+                ans.pop_back();
+            }
+            ans.push_back(s[i]);
+            mark[s[i] - 'a'] = true;
+        }
+        return ans;
+    }
+};
